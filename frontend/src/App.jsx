a971ea87935b19021ab15ddae75c9669f2683622
Mecklon <fernandesmecklon3@gmail.com>
2025-09-17 13:45:06 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,21 @@ import { useAuthContext } from "./hooks/useAuthContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const { username } = useAuthContext();
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
+      {!username ? (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="*" element={<div>App</div>}></Route>
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
