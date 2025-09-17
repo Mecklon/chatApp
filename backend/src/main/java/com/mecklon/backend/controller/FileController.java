fb0194d27a7cc00ccfe512b0b2366ad1b3acbee2
Mecklon <fernandesmecklon3@gmail.com>
2025-09-17 15:44:02 +0530

    updated-backend

---
+++ backend/src/main/java/com/mecklon/backend/controller/FileController.java
@@ -0,0 +1,32 @@
+package com.mecklon.backend.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    private final String uploadDir = "C:/Users/Mecklon Fernandes/Desktop/code/setup/backend/uploads";
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) throws MalformedURLException {
+        Path path = Paths.get(uploadDir).resolve(filename);
+        Resource resource = new UrlResource(path.toUri());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + filename + "\"")
+                .body(resource);
+    }
+}
+
